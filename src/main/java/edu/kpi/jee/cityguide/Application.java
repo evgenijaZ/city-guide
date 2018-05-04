39e41e56848f4b628b05e45e8c72978025c3de01
evgenijaZ <evg.zubrich@gmail.com>
2018-05-04 16:32:19 +0300

    added run class and properties

---
+++ src/main/java/edu/kpi/jee/cityguide/Application.java
@@ -0,0 +1,13 @@
+package edu.kpi.jee.cityguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
